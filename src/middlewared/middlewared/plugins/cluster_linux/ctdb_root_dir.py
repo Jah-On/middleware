1fd06d2d6c40014bb7c7b98ca0b9dd28f0c63d98
caleb <yocalebo@gmail.com>
2022-11-15 10:03:12 -0500

    ctdb.root_dir.setup replaces ctdb.shared.volume.create

--- src/middlewared/middlewared/plugins/cluster_linux/ctdb_root_dir.py
+++ src/middlewared/middlewared/plugins/cluster_linux/ctdb_root_dir.py
@@ -5,7 +5,7 @@ import pyglfs
 from middlewared.service import Service, CallError, job
 from .utils import CTDBConfig
 
-LOCK = 'ctdb-init-lock'
+LOCK = 'ctdb-setup-lock'
 ROOT_DIR_NAME = 'ctdb-root-dir'
 
 
@@ -15,20 +15,7 @@ class CtdbRootDirService(Service):
         namespace = 'ctdb.root_dir'
         private = True
 
-    @job(lock=LOCK)
-    def init(self, job, gvol_name):
-        """
-        This method will initialize the ctdb directory responsible for storing
-        files used by ctdb daemon for cluster operations. Without this directory,
-        ctdb daemon and therefore SMB active-active shares will not work.
-
-        We use the native gluster file I/O API via the pyglfs module. This means
-        we don't need to go through a local fuse mount.
-
-        Also, since this directory is stored at the root of the gluster volume, it's
-        imperative that we protect this directory via permissions. We lock this down
-        to only root user and nobody else may access it.
-        """
+    def create_dir_and_set_perms(self, gvol_name):
         root_handle = pyglfs.Volume({
             'volume_name': gvol_name,
             'volfile_servers': [{'host': '127.0.0.1', 'proto': 'tcp', 'port': 0}]
@@ -56,6 +43,64 @@ class CtdbRootDirService(Service):
         gid = 0 if stat.st_gid != 0 else -1
         if uid == 0 or gid == 0:
             dir_fd.fchown(uid, gid)
+
+    @job(lock=LOCK)
+    def setup(self, job, gvol_name, peers):
+        """
+        This method will initialize the ctdb directory responsible for storing
+        files used by ctdb daemon for cluster operations. Without this directory,
+        ctdb daemon and therefore SMB active-active shares will not work.
+
+        We use the native gluster file I/O API via the pyglfs module. This means
+        we don't need to go through a local fuse mount.
+
+        Also, since this directory is stored at the root of the gluster volume, it's
+        imperative that we protect this directory via permissions. We lock this down
+        to only root user and nobody else may access it.
+        """
+        # create the top level dir and set perms appropriately
+        self.create_dir_and_set_perms(gvol_name)
+
+        # The peers in the TSP could be using dns names while ctdb
+        # only accepts IP addresses. This means we need to resolve
+        # the hostnames of the peers in the TSP to their respective
+        # IP addresses so we can write them to the ctdb private ip file.
+        ips = self.middleware.call_sync('cluster.utils.resolve_hostnames', peers)
+        if len(peers) != len(ips):
+            # this means the gluster peers hostnames resolved to the same
+            # ip address which is bad....in theory, this shouldn't occur
+            # since adding gluster peers has it's own validation and would
+            # cause it to fail way before this gets called but it's better
+            # to be safe than sorry
+            raise CallError('Duplicate gluster peer IP addresses detected.')
+
+        # everything below here requires the local FUSE mount to be functional
+        # so let's be sure to wait for it if it's currently running
+        fuse_mount_job = self.middleware.call_sync('core.get_jobs', [
+            ('method', '=', 'gluster.fuse.mount'),
+            ('arguments.0.name', '=', gvol_name),
+            ('state', '=', 'RUNNING')
+        ])
+        if fuse_mount_job:
+            self.middleware.call_sync('core.job_wait', fuse_mount_job[0]['id']).wait_sync()
+
+        # ctdb daemon needs, minimally, private ips to be added to it's config
+        # before the service will start. This does a few things underneath the hood
+        #   1. adds private ips to the /etc/ctdb/nodes file
+        #   2. symlinks /etc/ctdb/nodes to /cluster/`gvol_name`/nodes
+        for peer in peers:
+            peer_job = await self.middleware.call('ctdb.private.ips.create', {'ip': peer})
+            await peer_job.wait()
+
+        # Initialize clustered secrets
+        if not self.middleware.call_sync('clpwenc.check'):
+            self.logger.debug('Generating clustered pwenc secret')
+            self.middleware.call_sync('clpwenc.generate_secret')
+
+        # finally, we need to send an event telling all peers in the TSP (including this system)
+        # to start the ctdb service
+        data = {'event': 'CTDB_START', 'name': gvol_name, 'forward': True}
+        await self.middleware.call('gluster.localevents.send', data)
 
     def set_location(self, gvol_name):
         """
--- src/middlewared/middlewared/plugins/cluster_linux/ctdb_shared_volume.py
+++ src/middlewared/middlewared/plugins/cluster_linux/ctdb_shared_volume.py
@@ -1,5 +1,5 @@
 import errno
-from pathlib import Path
+
 from glustercli.cli import volume
 
 from middlewared.service import Service, CallError, job
@@ -45,98 +45,14 @@ class CtdbSharedVolumeService(Service):
     @job(lock=CRE_OR_DEL_LOCK)
     async def create(self, job):
         """
-        Create and mount the shared volume to be used
-        by ctdb daemon.
+        We no longer have a gluster volume dedicated to ctdb but this
+        method has been left for backwards compatibility with TC api.
+
+        This is now a no-op by design. This plugin has always been private
+        so no one (ideally) should be consuming it. If they are, well, that's
+        the entire point of a private API...it can change without notice.
         """
-        # check if ctdb shared volume already exists and started
-        info = await self.middleware.call('gluster.volume.exists_and_started', CTDB_VOL_NAME)
-        if not info['exists']:
-            # get the peers in the TSP
-            peers = await self.middleware.call('gluster.peer.query')
-            if not peers:
-                raise CallError('No peers detected')
-
-            # shared storage volume requires 3 nodes, minimally, to
-            # prevent the dreaded split-brain
-            con_peers = [i['hostname'] for i in peers if i['connected'] == 'Connected']
-            if len(con_peers) < 3:
-                raise CallError(
-                    '3 peers must be present and connected before the ctdb '
-                    'shared volume can be created.'
-                )
-
-            # get the system dataset location
-            ctdb_sysds_path = (await self.middleware.call('systemdataset.config'))['path']
-            if not ctdb_sysds_path:
-                raise CallError("System dataset is not properly mounted")
-
-            ctdb_sysds_path = str(Path(ctdb_sysds_path).joinpath(CTDB_VOL_NAME))
-
-            bricks = []
-            for i in con_peers:
-                bricks.append(i + ':' + ctdb_sysds_path)
-
-            options = {'args': (CTDB_VOL_NAME, bricks,)}
-            options['kwargs'] = {'replica': len(con_peers), 'force': True}
-            await self.middleware.call('gluster.method.run', volume.create, options)
-
-        # make sure the shared volume is configured properly to prevent
-        # possibility of split-brain/data corruption with ctdb service
-        await self.middleware.call('ctdb.shared.volume.validate')
-
-        if not info['started']:
-            # start it if we get here
-            await self.middleware.call('gluster.volume.start', {'name': CTDB_VOL_NAME})
-
-        # try to mount it locally and send a request
-        # to all the other peers in the TSP to also
-        # FUSE mount it
-        data = {'event': 'VOLUME_START', 'name': CTDB_VOL_NAME, 'forward': True}
-        await self.middleware.call('gluster.localevents.send', data)
-
-        # we need to wait on the local FUSE mount job since
-        # ctdb daemon config is dependent on it being mounted
-        fuse_mount_job = await self.middleware.call('core.get_jobs', [
-            ('method', '=', 'gluster.fuse.mount'),
-            ('arguments.0.name', '=', 'ctdb_shared_vol'),
-            ('state', '=', 'RUNNING')
-        ])
-        if fuse_mount_job:
-            wait_id = await self.middleware.call('core.job_wait', fuse_mount_job[0]['id'])
-            await wait_id.wait()
-
-        # Initialize clustered secrets
-        if not await self.middleware.call('clpwenc.check'):
-            self.logger.debug('Generating clustered pwenc secret')
-            await self.middleware.call('clpwenc.generate_secret')
-
-        # The peers in the TSP could be using dns names while ctdb
-        # only accepts IP addresses. This means we need to resolve
-        # the hostnames of the peers in the TSP to their respective
-        # IP addresses so we can write them to the ctdb private ip file.
-        names = [i['hostname'] for i in await self.middleware.call('gluster.peer.query')]
-        ips = await self.middleware.call('cluster.utils.resolve_hostnames', names)
-        if len(names) != len(ips):
-            # this means the gluster peers hostnames resolved to the same
-            # ip address which is bad....in theory, this shouldn't occur
-            # since adding gluster peers has it's own validation and would
-            # cause it to fail way before this gets called but it's better
-            # to be safe than sorry
-            raise CallError('Duplicate gluster peer IP addresses detected.')
-
-        # Setup the ctdb daemon config. Without ctdb daemon running, none of the
-        # sharing services (smb/nfs) will work in an active-active setting.
-        priv_ctdb_ips = [i['address'] for i in await self.middleware.call('ctdb.private.ips.query')]
-        for ip_to_add in [i for i in ips if i not in [j for j in priv_ctdb_ips]]:
-            ip_add_job = await self.middleware.call('ctdb.private.ips.create', {'ip': ip_to_add})
-            await ip_add_job.wait()
-
-        # this sends an event telling all peers in the TSP (including this system)
-        # to start the ctdb service
-        data = {'event': 'CTDB_START', 'name': CTDB_VOL_NAME, 'forward': True}
-        await self.middleware.call('gluster.localevents.send', data)
-
-        return await self.middleware.call('gluster.volume.query', [('name', '=', CTDB_VOL_NAME)])
+        return
 
     @job(lock=CRE_OR_DEL_LOCK)
     async def teardown(self, job):
